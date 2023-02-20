a6c7b804949fd6ea0b3cb094d759b28252358bf2
Rexmann <kixmyth@yahoo.com>
2023-02-19 23:33:36 -0800

    Task 4 is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry Point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -8,5 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
-
+}
